f0298ce3f0616444a63f73ce284b97615604a5d5
Jenny <wils0986@algonquinlive.com>
2022-12-13 19:11:47 -0500

    adds animations to pages

--- js/main.js
+++ js/main.js
@@ -8,6 +8,17 @@ function myFunction() {
     }
   }
 
+
+  function myFunction() {
+    var x = document.getElementById("cat2");
+    if(x.classList.contains('show')){
+      x.classList.remove('show');
+    }else{
+      x.classList.add('show');
+    }
+  }
+
+
   /* Creates typewriter style text on Home page */
   var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
@@ -91,4 +102,23 @@ function switchTheme(e) {
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
-/* */
+/* Back to top button */
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
